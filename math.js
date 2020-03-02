b6a07aba0d8d373fe8c830dc9685a0f9c9981259
khondoker Eftakhar Jubayer <jubayer.kodeeo@gmail.com>
2020-03-02 13:28:52 +0600

    operator

---
+++ math.js
@@ -0,0 +1,36 @@
+//egg price
+/*
+var price = 5;
+var ammountOfEggs = 20;
+
+var totalPrice = price * ammountOfEggs;
+
+console.log("Total egg price", totalPrice, "taka");
+*/
+
+
+//area of a circle = pi * r squire
+
+/*
+var redius = 7;
+const pi = 3.14159;
+var areaOfCircle = pi * (redius * redius);
+
+console.log("Area of circle", areaOfCircle, "sq incs");
+*/
+
+//BDT to usd rate (1 USD = 83.5)
+/*
+var usdToBdt = 83.5;
+var BDT = 5000;
+
+var usd = BDT / usdToBdt;
+
+console.log(usd);
+*/
+
+//remainder 
+
+var n1 = 5
+var n2 = n1 % 2;
+console.log(n2);
